4ca39ab71848d0b919441a0b5949880e371732f9
Sr. Henrique <c.henrique.anjosdasilva@gmail.com>
2022-11-01 10:32:20 -0300

    universo

---
+++ prompt/prompt.js
@@ -0,0 +1,11 @@
+var idade = prompt("qual é sua idade? ")
+
+if (idade < 10){
+    console.log("você é criança");
+  }
+if (!idade < 18){
+    console.log("você é adolescente");
+  }
+else{
+    console.log("você é adulto")
+  }
